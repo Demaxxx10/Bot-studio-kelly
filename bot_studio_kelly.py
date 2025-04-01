b3cbf239bfac89cbc99c5ae127ae979698aef725
Demaxxx10 <maxwellcatalao@gmail.com>
2025-04-01 03:49:56 -0300

    Update bot_studio_kelly.py

--- bot_studio_kelly.py
+++ bot_studio_kelly.py
@@ -1,19 +1,19 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 VERIFY_TOKEN = "Studio2025"
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route("/webhook", methods=["GET", "POST"])
 def webhook():
-    if request.method == 'GET':
+    if request.method == "GET":
         token_sent = request.args.get("hub.verify_token")
         challenge = request.args.get("hub.challenge")
         if token_sent == VERIFY_TOKEN:
             return challenge
         return "Token inválido"
-
-import os
+    return "Webhook online!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
